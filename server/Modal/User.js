00acfa51c10183a5341917fbac9d19999c0fe156
Dhruv10143 <118088718+Dhruv10143@users.noreply.github.com>
2024-11-07 03:15:13 +0530

    Add files via upload

--- server/Modal/User.js
+++ server/Modal/User.js
@@ -1,51 +1,83 @@
 import mongoose from "mongoose";
 
-const userSchema=mongoose.Schema({
-    iss:{
-        type:String
+const userSchema = mongoose.Schema({
+    iss: {
+        type: String
     },
-    azp:{
-        type:String
+    azp: {
+        type: String
     },
-    aud:{
-        type:String
+    aud: {
+        type: String
     },
-    sub:{
-        type:String,
+    sub: {
+        type: String,
         required: true
     },
-    email:{
-        type:String
+    email: {
+        type: String,
+        required: true,
+        unique: true // Ensure unique emails
     },
-    email_verified:{
-        type:Boolean
+    email_verified: {
+        type: Boolean
     },
-    nbf:{
-        type:Number
+    nbf: {
+        type: Number
     },
-    given_name:{
-        type:String
+    given_name: {
+        type: String
     },
-    family_name:{
-        type:String
+    family_name: {
+        type: String
     },
-    iat:{
-        type:Number
+    iat: {
+        type: Number
     },
-    exp:{
-        type:Number
+    exp: {
+        type: Number
     },
-    jti:{
-        type:String
+    jti: {
+        type: String
     },
-    name:{
-        type:String,
-        required:true
-    },
-    picture:{
-        type:String,
+    name: {
+        type: String,
         required: true
+    },
+    picture: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    password: {
+        type: String, // Add password for manual registration
+        required: function () {
+            return this.password || false; // Make sure password is only required for manual registration
+        }
     },
 });
-const user= mongoose.model('user',userSchema);
-export default user;
+
+// Add a method to hash the password before saving
+userSchema.pre("save", function (next) {
+    if (this.password) {
+        // Hash the password before saving it (use a hashing library like bcrypt)
+        const bcrypt = require("bcrypt");
+        bcrypt.hash(this.password, 10, (err, hashedPassword) => {
+            if (err) {
+                return next(err);
+            }
+            this.password = hashedPassword; // Store the hashed password
+            next();
+        });
+    } else {
+        next();
+    }
+});
+
+const User = mongoose.model('user', userSchema);
+export default User;
