439726cb8d9aa31baa938fb294835fcb53ab8b2f
Dhruv10143 <118088718+Dhruv10143@users.noreply.github.com>
2024-11-07 03:18:29 +0530

    Add files via upload

--- socket/index.js
+++ socket/index.js
@@ -6,19 +6,35 @@ const io=new Server(9000,{
 })
 let users=[];
 const addUser=(userData,socketId)=>{
-     !users.some(user=>user.sub == userData.sub) && users.push({ ...userData, socketId});
+    !users.some(user=>user.sub == userData.sub) && users.push({ ...userData, socketId});
 }
+
+const remuser=(sockId)=>{
+    const newusers = users.filter((user) =>  user.socketId !== sockId);
+    users = newusers;
+}
+
 const getUser=(userId)=>{
-          return users.find(user=>user.sub===userId);
+    return users.find(user=>user.sub===userId);
 }
 io.on('connection',(socket)=>{
     console.log('user connected');
+    socket.on("disconnect", () => {
+        io.emit("user_disconnected", socket.id);
+        remuser(socket.id);
+        io.emit("getUsers",users);
+    })
+
+    io.on("emitData", account => {
+        io.emit("newDevice", {user:account, sockId:socket.id});
+    })
+    
     socket.on("addUsers",userData=>{
         addUser(userData,socket.id);
         io.emit("getUsers",users);
     });
     socket.on('sendMessage',data=>{
-        const user=getUser(data.recieverId);
-        io.to(user.socketId).emit('getMessage',data);
+        const user = getUser(data.receiverId);
+        user ? io.to(user.socketId).emit('getMessage',data) : console.log("User is offline");
     })
 })
